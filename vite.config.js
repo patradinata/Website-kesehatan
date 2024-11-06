ebae50d522811bd3a2308b3af347f68af395fb8b
joanpurbaa <joanpurba562@gmail.com>
2024-11-06 08:32:49 +0700

    fix : remove comment form vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import envCompatible from "vite-plugin-env-compatible";
 
-// https://vite.dev/config/
 export default defineConfig({
   envPrefix: "VITE_",
   plugins: [react(), envCompatible()],
