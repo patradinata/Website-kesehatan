7624c907bf6b58ae9f17c6a6d77e74ac2495589d
joanpurbaa <joanpurba562@gmail.com>
2024-11-06 20:20:20 +0700

    fix : change the default router UI design

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,91 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
+import { BsX } from "react-icons/bs";
+import { IoIosArrowDropup } from "react-icons/io";
+import Header from "./components/Header";
 
 const Home = () => {
+  const [promoStatus, setPromoStatus] = useState(true);
+  const [toTopActive, toTopStatus] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    window.scrollY >= 100 ? toTopStatus(true) : toTopStatus(false);
+  });
+
   return (
     <>
-      <div className="grid grid-cols-12 grid-rows-12 gap-5 p-5 h-screen">
-        <Link to="/imt" className="flex justify-center items-center bg-gradient-to-r from-red-400 to-red-700 hover:from-red-600 hover:to-red-600 col-span-12 sm:col-span-6 row-span-3 sm:row-span-6 rounded-lg cursor-pointer">
-          <h1 className="text-xl sm:text-3xl font-bold text-white text-center">BMI (Body mass index)</h1>
-        </Link>
-        <Link to="/cardgame" className="flex justify-center items-center bg-gradient-to-r from-yellow-400 to-yellow-700 hover:from-yellow-600 hover:to-yellow-600 col-span-12 sm:col-span-6 row-span-3 sm:row-span-6 rounded-lg cursor-pointer">
-          <h1 className="text-xl sm:text-3xl font-bold text-white text-center">Memory card game</h1>
-        </Link>
-        <Link to="/tinggiideall" className="flex justify-center items-center bg-gradient-to-r from-green-400 to-green-700 hover:from-green-600 hover:to-green-600 col-span-12 sm:col-span-6 row-span-3 sm:row-span-6 rounded-lg cursor-pointer">
-          <h1 className="text-xl sm:text-3xl font-bold text-white text-center">Tinggi badan ideal</h1>
-        </Link>
-        <Link to="/prediksikelahiran" className="flex justify-center items-center bg-gradient-to-r from-blue-400 to-blue-700 hover:from-blue-600 hover:to-blue-600 col-span-12 sm:col-span-6 row-span-3 sm:row-span-6 rounded-lg cursor-pointer">
-          <h1 className="text-xl sm:text-3xl font-bold text-white text-center">Prediksi kelahirkan</h1>
-        </Link>
+      <div id="home" className="w-full h-full bg-gradient-to-b from-red-600 to-red-400">
+        {promoStatus == true ? (
+          <div className="w-full bg-gradient-to-r from-yellow-400 to-yellow-500  py-2 es:px-1 flex flex-row justify-around items-center">
+            <div></div>
+            <p className="text-zinc-700 text-md">
+              Kontak nomor <b>112</b> dan <b>119</b> untuk menelpon{" "}
+              <b>ambulans!!</b>
+            </p>
+            <BsX
+              className="text-zinc-700 text-2xl cursor-pointer"
+              onClick={() => setPromoStatus(false)}
+            />
+          </div>
+        ) : (
+          <div className="hidden"></div>
+        )}
+        <Header />
+        <div className="flex flex-col justify-center items-center gap-y-5 py-40">
+          <p className="text-white text-center">Explore sesuka hati!</p>
+          <h1 className="text-white text-6xl font-black text-center">
+            Akses <span className="text-yellow-300">Kesehatan</span> Berkualitas
+          </h1>
+          <TypeAnimation
+            className="text-white font-black text-6xl"
+            sequence={[
+              "untuk Hidup Lebih Sehat",
+              1000,
+              "untuk Hidup Lebih Sejahtera",
+              1000,
+              "untuk Hidup Lebih Baik",
+              1000,
+              "Tunggu apa lagi? ayo explore!!",
+              1000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+          />
+          <p className="text-white text-center">
+            Game, dan Kalkulator kesehatan untuk hidup yang lebih sejahtera!
+          </p>
+        </div>
+        <div
+          id="tujuan"
+          className="grid grid-cols-12 items-center text-white py-40"
+        >
+          <div className="col-start-3 col-span-4 flex justify-center items-center">
+            <img className="relative w-full" src="/tujuanKami.png" alt="" />
+          </div>
+          <div className="col-start-8 col-span-3">
+            <h1 className="text-5xl font-black">
+              Menyejahterakan banyak orang!
+            </h1>
+            <p className="mt-5 text-lg">
+              Memastikan setiap individu, tanpa memandang usia, mendapatkan hak
+              atas kesehatan yang layak dan kesejahteraan yang optimal melalui
+              akses layanan kesehatan yang merata dan berkualitas.
+            </p>
+          </div>
+        </div>
+        {toTopActive ? (
+          <a href="#home">
+            <IoIosArrowDropup className="fixed z-10 right-5 bottom-5 sm:right-10 sm:bottom-10 w-12 h-12 bg-white rounded-md p-2 shadow-xl text-zinc-700" />
+          </a>
+        ) : (
+          <a href="#home">
+            <IoIosArrowDropup className="invisible fixed right-5 bottom-5 sm:right-10 sm:bottom-10 w-10 h-10 text-zinc-700" />
+          </a>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+const Header = () => {
+  return (
+    <>
+      <header className="flex justify-center py-5">
+        <nav>
+          <ul className="flex gap-x-32 text-white">
+            <li>
+              <a href="#tujuan">Tujuan</a>
+            </li>
+            <li>
+              <a href="">Layanan</a>
+            </li>
+            <li>
+              <a href="">Game</a>
+            </li>
+            <li>
+              <a target="_blank" href="https://api.whatsapp.com/send/?phone=62882000561667&text=Hai+Joan+mau+nanya+dong&type=phone_number&app_absent=0">
+                Kontak
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
